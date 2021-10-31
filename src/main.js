c061c1e4e4eb90e7971281c65292eca8cc6707ec
Azeem Vasanwala <azeem.iron@gmail.com>
2021-10-31 13:48:05 +0800

    Update fontawesome

--- src/main.js
+++ src/main.js
@@ -1,17 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
-import axios from "axios";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faUpload } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 library.add(faUpload);
 Vue.component("vue-fontawesome", FontAwesomeIcon);
-
-axios.defaults.baseURL =
-    process.env.VUE_APP_BASE_URL || "http://localhost:3000";
-
-Object.defineProperty(Vue.prototype, "$axios", { value: axios });
 
 import Buefy from "buefy";
 import "buefy/dist/buefy.css";
